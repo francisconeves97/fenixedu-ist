f471c93b1dcbd942b8073eb9b92bd1075b328477
Francisco Neves <francisco.neves@tecnico.ulisboa.pt>
2018-07-19 16:33:37 +0100

    Include author object

--- fenixedu-ist-integration/src/main/java/pt/ist/fenixedu/integration/api/FenixCourseAnnouncement.java
+++ fenixedu-ist-integration/src/main/java/pt/ist/fenixedu/integration/api/FenixCourseAnnouncement.java
@@ -1,26 +1,62 @@
 package pt.ist.fenixedu.integration.api;
 
 import com.fasterxml.jackson.annotation.JsonRawValue;
+import org.fenixedu.academic.domain.Person;
 import org.fenixedu.cms.domain.Post;
-import org.joda.time.DateTime;
 
 public class FenixCourseAnnouncement {
     private String name;
     private String body;
     private String link;
-    private String author = "";
-    private String authorId = "";
+    private FenixAuthor author;
     private String guid;
     private String publicationDate;
+
+    private class FenixAuthor {
+
+        private String name;
+        private String email;
+        private String username;
+
+        private FenixAuthor(String name, String email, String username) {
+            this.name = name;
+            this.email = email;
+            this.username = username;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+    }
 
     public FenixCourseAnnouncement(Post post) {
         this.name = post.getName().json().toString();
         this.body = post.getBody().json().toString();
         this.link = post.getAddress();
-        if (post.getCreatedBy().getProfile() != null && post.getCreatedBy().getProfile().getEmail() != null
-                && post.getCreatedBy().getProfile().getPerson() != null) {
-            this.author = post.getCreatedBy().getProfile().getEmail();
-            this.authorId = post.getCreatedBy().getProfile().getPerson().getUsername();
+        if (post.getCreatedBy().getProfile() != null && post.getCreatedBy().getProfile().getPerson() != null) {
+            Person person = post.getCreatedBy().getProfile().getPerson();
+
+            this.author = new FenixAuthor(person.getDisplayName(), post.getCreatedBy().getEmail(), person.getUsername());
         }
         this.guid = post.getAddress() + "#" + post.getExternalId();
         this.publicationDate = post.getCreationDate().toString("yyyy-MM-dd HH:mm:ss");
@@ -52,20 +88,8 @@ public class FenixCourseAnnouncement {
         this.link = link;
     }
 
-    public String getAuthor() {
+    public FenixAuthor getAuthor() {
         return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getAuthorId() {
-        return authorId;
-    }
-
-    public void setAuthorId(String authorId) {
-        this.authorId = authorId;
     }
 
     public String getGuid() {
